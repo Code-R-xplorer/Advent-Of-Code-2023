b1b88f9ff79ca93b4bfd3e54196356808f4d6f20
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-12-02 13:17:23 +0000

    Part 1

--- src/Day_2.py
+++ src/Day_2.py
@@ -2,3 +2,54 @@ from utils import read_file
 
 values = read_file(2, str, True)
 
+games = []
+
+
+class Game:
+    def __init__(self, ID, rounds):
+        self.ID = ID
+        self.rounds = rounds
+
+
+class Round:
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+
+for value in values:
+    gameNumber, subsetList = value.split(': ')
+    gameID = int(gameNumber.split(' ')[1])
+    roundsList = subsetList.split('; ')
+    gameRounds = []
+    for r in roundsList:
+        currentRed, currentGreen, currentBlue = 0, 0, 0
+        cubesList = r.split(', ')
+        for cube in cubesList:
+            count, colour = cube.split(' ')
+            if colour == 'red':
+                currentRed += int(count)
+            if colour == 'green':
+                currentGreen += int(count)
+            if colour == 'blue':
+                currentBlue += int(count)
+        currentRound = Round(currentRed, currentGreen, currentBlue)
+        gameRounds.append(currentRound)
+    game = Game(gameID, gameRounds)
+    games.append(game)
+
+bagRed, bagGreen, bagBlue = 12, 13, 14
+
+gameIDSum = 0
+
+for game in games:
+    gameValid = True
+    for gameRound in game.rounds:
+        if gameRound.red > bagRed or gameRound.green > bagGreen or gameRound.blue > bagBlue:
+            gameValid = False
+    if gameValid:
+        gameIDSum += game.ID
+
+# Part 1 1931
+print(gameIDSum)
