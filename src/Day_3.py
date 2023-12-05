03e4a8e91f688e2bae51e96f2403b44e84c92423
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-12-05 12:38:55 +0000

    Part 2

--- src/Day_3.py
+++ src/Day_3.py
@@ -5,7 +5,27 @@ values = read_file(3, str, False)
 
 Position = namedtuple('Position', ['x', 'y'])
 
+
+class Gear:
+    def __init__(self, pos: Position):
+        self.pos = pos
+        self.connected_parts = 0
+        self.part_numbers = []
+
+    def connect_part(self, part_number):
+        self.connected_parts += 1
+        self.part_numbers.append(part_number)
+
+    def get_connected_parts(self) -> int:
+        return self.connected_parts
+
+    def get_gear_ratio(self) -> int:
+        return self.part_numbers[0] * self.part_numbers[1]
+
+
 Grid = []
+gears = {}
+
 
 Symbols = "*@#-=/+%$&"
 
@@ -21,12 +41,16 @@ class EngineNumber:
         if self.pos.y != len(Grid) - 1:
             for i in range(self.length):
                 if any(c in Symbols for c in Grid[self.pos.y + 1][self.pos.x + i]):
+                    if Grid[self.pos.y + 1][self.pos.x + i] == '*':
+                        gears.get(Position(self.pos.x + i, self.pos.y + 1)).connect_part(self.number)
                     return True
 
         # Above
         if self.pos.y != 0:
             for i in range(self.length):
                 if any(c in Symbols for c in Grid[self.pos.y - 1][self.pos.x + i]):
+                    if Grid[self.pos.y - 1][self.pos.x + i] == '*':
+                        gears.get(Position(self.pos.x + i, self.pos.y - 1)).connect_part(self.number)
                     return True
 
         return False
@@ -35,11 +59,15 @@ class EngineNumber:
         # Left
         if self.pos.x != 0:
             if any(c in Symbols for c in Grid[self.pos.y][self.pos.x - 1]):
+                if Grid[self.pos.y][self.pos.x - 1] == '*':
+                    gears.get(Position(self.pos.x - 1, self.pos.y)).connect_part(self.number)
                 return True
 
         # Right
         if (self.pos.x + self.length - 1) != len(Grid[0]) - 1:
             if any(c in Symbols for c in Grid[self.pos.y][(self.pos.x + self.length - 1) + 1]):
+                if Grid[self.pos.y][(self.pos.x + self.length - 1) + 1] == '*':
+                    gears.get(Position((self.pos.x + self.length - 1) + 1, self.pos.y)).connect_part(self.number)
                 return True
 
         return False
@@ -48,21 +76,29 @@ class EngineNumber:
         # Upper Left
         if self.pos.x != 0 and self.pos.y != 0:
             if any(c in Symbols for c in Grid[self.pos.y - 1][self.pos.x - 1]):
+                if Grid[self.pos.y - 1][self.pos.x - 1] == '*':
+                    gears.get(Position(self.pos.x - 1, self.pos.y - 1)).connect_part(self.number)
                 return True
 
         # Upper Right
         if (self.pos.x + self.length - 1) != len(Grid[0]) - 1 and self.pos.y != 0:
             if any(c in Symbols for c in Grid[self.pos.y - 1][(self.pos.x + self.length - 1) + 1]):
+                if Grid[self.pos.y - 1][(self.pos.x + self.length - 1) + 1] == '*':
+                    gears.get(Position((self.pos.x + self.length - 1) + 1, self.pos.y - 1)).connect_part(self.number)
                 return True
 
         # Lower Left
         if self.pos.x != 0 and self.pos.y != len(Grid) - 1:
             if any(c in Symbols for c in Grid[self.pos.y + 1][self.pos.x - 1]):
+                if Grid[self.pos.y + 1][self.pos.x - 1] == '*':
+                    gears.get(Position(self.pos.x - 1, self.pos.y + 1)).connect_part(self.number)
                 return True
 
         # Lower Right
         if (self.pos.x + self.length - 1) != len(Grid[0]) - 1 and self.pos.y != len(Grid) - 1:
             if any(c in Symbols for c in Grid[self.pos.y + 1][(self.pos.x + self.length - 1) + 1]):
+                if Grid[self.pos.y + 1][(self.pos.x + self.length - 1) + 1] == '*':
+                    gears.get(Position((self.pos.x + self.length - 1) + 1, self.pos.y + 1)).connect_part(self.number)
                 return True
 
         return False
@@ -71,9 +107,11 @@ class EngineNumber:
         return self.check_vertical() or self.check_next_to() or self.check_diagonal()
 
 
+
 current_y = 0
 
 engine_numbers = []
+
 
 for value in values:
     line = [*value]
@@ -82,6 +120,8 @@ for value in values:
     current_number_str = ""
 
     for count, character in enumerate(line):
+        if character == '*':
+            gears[Position(count, current_y)] = Gear(Position(count, current_y))
         if character.isnumeric():
             if current_number_str == "":
                 starting_pos = Position(count, current_y)
@@ -93,15 +133,9 @@ for value in values:
             engine_numbers.append(EngineNumber(int(current_number_str), starting_pos, len(current_number_str)))
             current_number_str = ""
         if count == len(line) - 1 and len(current_number_str) > 0:
-            print(starting_pos)
             engine_numbers.append(EngineNumber(int(current_number_str), starting_pos, len(current_number_str)))
             current_number_str = ""
     current_y += 1
-
-# for engine in engine_numbers:
-#     print(f'Number(value={engine.number}, start_position=({engine.pos.x}, {engine.pos.y}), length={engine.length})')
-
-print(len(engine_numbers))
 
 total = 0
 
@@ -111,3 +145,15 @@ for engineNumber in engine_numbers:
 
 # Part 1 = 544664
 print(total)
+
+gear_ratio_sum = 0
+
+for position in gears:
+    if gears[position].get_connected_parts() == 2:
+        gear_ratio_sum += gears[position].get_gear_ratio()
+
+# Part 2 = 84495585
+print(gear_ratio_sum)
+
+
+
