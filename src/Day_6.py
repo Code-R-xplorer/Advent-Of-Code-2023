3693e56d43f5a5f4cffc19d5720524733a601636
Harry Vallance <harry@robvallance.com>
2023-12-06 13:29:41 +0000

    Part 2

--- src/Day_6.py
+++ src/Day_6.py
@@ -37,5 +37,30 @@ def multiply_list(_list):
         result = result * x
     return result
 
+
 # Part 1 = 2612736
 print(multiply_list(beat_record_totals))
+
+# Part 2
+
+new_time = ""
+for time in times:
+    new_time += str(time)
+
+new_distance = ""
+for distance in distances:
+    new_distance += str(distance)
+
+new_time = int(new_time)
+new_distance = int(new_distance)
+
+new_beaten_record = 0
+# Very slow and needs to be improved later
+for j in range(new_time + 1):
+    time_left = new_time - j
+    distance_covered = time_left * j
+    if distance_covered > new_distance:
+        new_beaten_record += 1
+
+# Part 2 = 29891250
+print(new_beaten_record)
